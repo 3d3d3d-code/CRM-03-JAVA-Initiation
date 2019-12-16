1e57e6c3995a39fe593e64ab81e06ec5ed8a2db7
3d3d3d-code <reda.hamza8@orange.fr>
2019-12-16 16:55:15 +0100

    java doc

--- Projet1/src/ExerciceJava.java
+++ Projet1/src/ExerciceJava.java
@@ -1,17 +1,35 @@
-/**
- * 
- */
 
 /**
  * @author CRM
+ * @since 2019 
+ * @version 1.0
  *
  */
 public class ExerciceJava {
 
 	/**
+	 * @author
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	
+	
+	
+
+	{
+		
+System.out.println("hello!");
+
+	
+	
+	
+		
+		
+		
+        
+			
+		
+		
 		// TODO Auto-generated method stub
 
 	}
