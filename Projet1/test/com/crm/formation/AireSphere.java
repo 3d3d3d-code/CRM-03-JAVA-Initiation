a346f4c3be704fc29330fac31889b4d958c0e32a
3d3d3d-code <reda.hamza8@orange.fr>
2020-01-06 11:07:59 +0100

    aire et volume sphere

---
+++ Projet1/test/com/crm/formation/AireSphere.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+import java.util.Scanner;
+
+/**
+ * @author CRM
+ *
+ */
+public class AireSphere {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		double R = 0;
+		double pi = 3.14;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("entrez le rayon de la sphere");
+
+		R = sc.nextDouble();
+		System.out.println("le rayon est de" + R);
+		double aire = 4*pi*R*R;
+		System.out.println("l'aire de la sphere est de :" + aire +" cm2");
+		double volume =4/3*pi*R*R*R;
+		System.out.println("et son volume est de " + volume + "cm3");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
