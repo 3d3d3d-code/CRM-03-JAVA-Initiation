38020d3bcc12cd6cd7d5097b3b17056f3e811b5d
3d3d3d-code <reda.hamza8@orange.fr>
2020-01-07 16:00:17 +0100

    perimetre methode

--- Projet1/test/com/crm/formation/MethodeTest.java
+++ Projet1/test/com/crm/formation/MethodeTest.java
@@ -13,19 +13,30 @@ public class MethodeTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int nb1=2;
-		int nb2=3;
-		int s1 = somme(nb1,nb2);
-		System.out.println(s1);
-		
-	}
-		public static int somme (int a, int b) {
-			int s= a+b;
-			return s;
-	
-		}
-		// TODO Auto-generated method stub
 
-	
+		int nb1 = 2;
+		int nb2 = 3;
+		int s1 = somme(nb1, nb2);
+		System.out.println(s1);
+
+		float l = 4;
+		float L = 7;
+		float P = perimetre(l, L);
+		
+		System.out.println(P);
+	}
+
+	public static int somme(int a, int b) {
+		int s = a + b;
+		return s;
+
+	}
+
+	public static float perimetre(float l, float L) {
+		float P = (l + L) * 2;
+		return P;
+
+	}
+	// TODO Auto-generated method stub
 
 }
