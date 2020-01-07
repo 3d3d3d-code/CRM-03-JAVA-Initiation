2f1f9d2deb55903fdb697340ce3349c989993866
3d3d3d-code <reda.hamza8@orange.fr>
2020-01-07 09:54:17 +0100

    operateur relationnel

---
+++ Projet1/test/com/crm/formation/OperateurRelationnel.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class OperateurRelationnel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+		
+		int var1= 7;
+		int var2=13;
+		boolean res = true;
+		res=(var1==var2);
+		
+		var1=var2;
+		
+		res=(var2<var2);
+		
+		System.out.println(res);
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
