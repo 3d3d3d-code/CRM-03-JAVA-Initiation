5ae43e940452063c97db418dbc95da1669360f1a
3d3d3d-code <reda.hamza8@orange.fr>
2020-01-07 12:42:47 +0100

    BOOLEAN

---
+++ Projet1/test/com/crm/formation/BooleanTest.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+
+/**
+ * @author CRM
+ *
+ */
+public class BooleanTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		boolean bool1 = true;
+		boolean bool2 = false;
+		/*boolean bool3 = bool1 && bool2;
+		boolean bool4 = bool1 || bool2;
+		boolean bool5 = bool1 & bool2 ;
+		boolean bool6 = bool1 | bool2 ;
+		boolean bool7 = bool1 ^ bool2 ;*/
+		short nb1,nb2,nb3;
+		nb1=1;
+		nb2=2;
+		nb3=3;
+		
+		boolean bool8 = nb1>nb2;
+		boolean bool9 =( nb1==nb2);
+		boolean bool10 = bool8||bool9;
+	
+	
+		System.out.println(bool10);
+		
+		
+	
+		
+		
+		
+		
+		
+	
+	/*	System.out.println(bool4);
+		System.out.println(bool4);
+		System.out.println(bool4);
+		
+		
+
+		System.out.println(bool3);
+		System.out.println(bool4);
+		System.out.println(bool5);
+		System.out.println(bool6);
+		System.out.println(bool7);
+	/*	System.out.println(bool8);
+		System.out.println(bool9);*/
+		
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
