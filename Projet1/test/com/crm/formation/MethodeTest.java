8c4084c84cbfe22fe7db288e1da118006ee2bbc7
3d3d3d-code <reda.hamza8@orange.fr>
2020-01-07 14:57:58 +0100

    tout

---
+++ Projet1/test/com/crm/formation/MethodeTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class MethodeTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int nb1=2;
+		int nb2=3;
+		int s1 = somme(nb1,nb2);
+		System.out.println(s1);
+		
+	}
+		public static int somme (int a, int b) {
+			int s= a+b;
+			return s;
+	
+		}
+		// TODO Auto-generated method stub
+
+	
+
+}
