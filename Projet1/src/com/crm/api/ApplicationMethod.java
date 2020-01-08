f06e8e7adf55954cbeccc5baad3ec0b0002f1f2d
3d3d3d-code <reda.hamza8@orange.fr>
2020-01-08 15:43:12 +0100

    dernier 8/01/20

---
+++ Projet1/src/com/crm/api/ApplicationMethod.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.crm.api;
+
+/**
+ * @author CRM
+ *
+ */
+public class ApplicationMethod {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int nb=6;
+		int nb1 =9;
+		
+		int maxnb = Math.max( nb , nb1);
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
