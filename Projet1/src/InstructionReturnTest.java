8c5c79685e201c46d8235462c35d88e8da9d1054
3d3d3d-code <reda.hamza8@orange.fr>
2020-01-16 12:54:30 +0100

    Nombre aleatoire

---
+++ Projet1/src/InstructionReturnTest.java
@@ -0,0 +1,22 @@
+
+public class InstructionReturnTest {
+
+	public static void main(String[] args) {
+		System.out.println("Debut methode main ");
+		int nombre = 6;
+		afficherListeNombre(nombre);
+		System.out.println("Fin methode main ");
+	}
+
+	public static void afficherListeNombre(int nb) {
+		System.out.println("Debut methode afficherListeNombre ");
+		for (int i = 0; i < nb; i++) {
+			if (i == 5) {				
+				return;
+			}
+			System.out.println(i);
+		}
+		System.out.println("fin methode afficherListeNombre ");
+
+	}
+}
--- Projet1/src/com/crm/api/NombreAleatoire.java
+++ Projet1/src/com/crm/api/NombreAleatoire.java
@@ -7,19 +7,27 @@ public class NombreAleatoire {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-     	int i=0;
-		int nb=0;
-		
-		System.out.println("entrer un nombre");
-		nb=sc.nextInt();
-		
-		do(nb<=n){
-			
-			
-			
-			
-			while}
-	}
 
+		int nbDeviner = 5;
+		int nb = 0;
+		int compt = 0;
+		System.out.println("entrer un nombre entre 1 et 10");
+		while (nb != nbDeviner && compt < 3) {
+
+			nb = sc.nextInt();
+			compt++;
+			if (nb == nbDeviner) {
+				System.out.println("super tu as trouvé en " + compt + " essais");
+			} else if (compt >= 3) {
+				System.out.println("tu as perdu");
+
+			} else {
+
+				System.out.println("Cherchez encore");
+
+			}
+
+		}
+
+	}
 }
