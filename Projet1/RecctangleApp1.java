9bff264017a417353999355a51cfd3af42331f1e
3d3d3d-code <reda.hamza8@orange.fr>
2020-02-10 08:02:16 +0100

    dernier commit rectangle java

---
+++ Projet1/RecctangleApp1.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * @author CRM
+ * @version 1.0
+ * @created 07-févr.-2020 09:36:09
+ */
+public class RecctangleApp1 {
+
+	private int rect1.setLargeur;
+
+	public RecctangleApp1(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
